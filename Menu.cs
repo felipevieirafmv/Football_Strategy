a35d09d434e2a43565be71f5e65607c4c14a88c4
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2024-01-11 16:48:04 -0300

    Comeco da tela de login

---
+++ Menu.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class Menu : Form
+{
+    private Graphics g = null;
+    private Bitmap bmp = null;
+    private Image img = null;
+    private PictureBox pb = new PictureBox {
+        Dock = DockStyle.Fill,
+    };
+    public Menu()
+    {
+        this.Load += delegate
+        {
+            WindowState = FormWindowState.Maximized;
+            this.Text = "Joguinho";
+
+            Button ngBtn = new Button();
+            ngBtn.Text = "New Game";
+            ngBtn.Font = new Font("Comic Sans MS", 30);
+            ngBtn.Width = 200;
+            ngBtn.Height = 70;
+            ngBtn.Location = new Point(1500, 400);
+            ngBtn.Click += delegate
+            {
+                this.Hide();
+
+            };
+
+            Button exBtn = new Button();
+            exBtn.Text = "Exit";
+            exBtn.Font = new Font("Comic Sans MS", 30);
+            exBtn.Width = 200;
+            exBtn.Height = 70;
+            exBtn.Location = new Point(1500, 600);
+            exBtn.Click += delegate
+            {
+                Application.Exit();
+            };
+
+            bmp = new Bitmap(
+                pb.Width,
+                pb.Height
+            );
+
+            // this.img = Image.FromFile("Img/suarezPixelado.png");
+
+            this.g = Graphics.FromImage(bmp);
+
+            // this.g.DrawImage(bmp, 0, 0);
+
+            Controls.Add(ngBtn);
+            Controls.Add(exBtn);
+        };
+    }
+}
